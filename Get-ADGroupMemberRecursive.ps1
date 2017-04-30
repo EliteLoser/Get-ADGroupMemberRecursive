19b2db4c5cb8f4f323681cf346dfa67bba449cbf
vayaconspade <joakimbs@gmail.com>
2017-04-30 02:13:20 +0200

    Uploading initial version

---
+++ Get-ADGroupMemberRecursive.ps1
@@ -0,0 +1,70 @@
+﻿#requires -version 3
+function Get-ADGroupMemberRecursive {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Mandatory = $true)]
+        [Alias("DistinguishedName", "Name", "SamAccountName")]
+        [String]
+        $Identity,
+        [String[]]
+        [Alias("Properties")]
+        $Property = @("DistinguishedName", "Name", "SamAccountName", "DisplayName"),
+        [Switch] $AsJSON)
+    begin {
+        Import-Module -Name ActiveDirectory -ErrorAction Stop -Verbose:$false
+        $Groups = @{}
+        function Get-ADGroupMemberInternal {
+            param(
+                [String] $Identity)
+            foreach ($Member in @(Get-ADGroupMember -Identity $Identity)) {
+                Write-Verbose -Message "[$($Member.DistinguishedName)] Processing ..."
+                if ($Member.ObjectClass -eq 'Group') {
+                    if ($Groups.ContainsKey($Member.DistinguishedName)) {
+                        Write-Verbose -Message "[$($Member.DistinguishedName)] Already processed."
+                        continue # explicit..
+                    }
+                    else {
+                        Write-Verbose -Message "[$($Member.DistinguishedName)] Processing group. Parent group: $Identity."
+                        $Groups[$Member.DistinguishedName] = @()
+                        Get-ADGroupMemberInternal -Identity $Member.DistinguishedName #-ParentGroup $Member.DistinguishedName
+                    }
+                }
+                else {
+                    Write-Verbose -Message "[$($Member.DistinguishedName)] Adding non-group element to $Identity array."
+                    if ($Groups.ContainsKey($Identity)) {
+                        $Groups[$Identity] += @($Member |
+                            Add-Member -MemberType NoteProperty -Name DirectParentGroupDN -Value $Identity -PassThru -Force |
+                            Select-Object -Property @(@($Property) + @("DirectParentGroupDN")))
+                    }
+                }
+            }
+        }
+    }
+    process {
+        if (-not ($GrandParentDN = (Get-ADGroup $_ -ErrorAction SilentlyContinue).DistinguishedName)) {
+            Write-Error -Message "[$_] Supplied identity is not a group." -ErrorAction Stop
+            return # acts as a continue in foreach loops?
+        }
+        $Groups[$GrandParentDN] = @()
+        Get-ADGroupMemberInternal -Identity $GrandParentDN
+    }
+    end {
+        if ($AsJson) {
+            $Groups.GetEnumerator() | ForEach-Object {
+                $Group = $_.Name
+                $_.Value | Select-Object -Property @{ Name = "RootGroupDN"; Expression = { $GrandParentDN } }, *
+            } | ConvertTo-Json
+        }
+        else {
+            $Groups.GetEnumerator() | ForEach-Object {
+                $Group = $_.Name
+                $_.Value | Select-Object -Property @{ Name = "RootGroupDN"; Expression = { $GrandParentDN } }, *
+            }
+        }
+        ## DEBUG ##
+        Write-Verbose -Message "Exporting main data hash to `$Global:STGroupHashTemp."
+        $Global:STGroupHashTemp = $Groups
+    }
+}
