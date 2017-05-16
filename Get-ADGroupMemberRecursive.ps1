8786141c92a79972ece949574b32037f267b9e09
Joakim Svendsen <joakimbs@gmail.com>
2017-05-16 23:59:45 +0200

    Comment update (easy to make v2-compatible)

--- Get-ADGroupMemberRecursive.ps1
+++ Get-ADGroupMemberRecursive.ps1
@@ -33,6 +33,9 @@ function Get-ADGroupMemberRecursive {
                 }
                 else {
                     Write-Verbose -Message "[$($Member.DistinguishedName)] Adding non-group element to $Identity array."
+                    # NB. That select-object below and the array concatenation of parameters is the only thins
+                    # that requires PSv3, so if you have v2, you can hardcode properties here and remove the "#requires -version 3"
+                    # at the top.
                     if ($Groups.ContainsKey($Identity)) {
                         $Groups[$Identity] += @($Member |
                             Add-Member -MemberType NoteProperty -Name DirectParentGroupDN -Value $Identity -PassThru -Force |
